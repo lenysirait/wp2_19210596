f5167803d65e54e5c83febbcc7419cfd9063897c
lenysirait <leniisirait03@gmail.com>
2022-09-26 23:26:15 -0700

    Menambahkan File wp2_19210596

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,14 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+	public function index()
+	{
+		echo "<h1>Perkenalan</h1>";
+		echo "
+		Nama Saya Leny Sindora Sirait <br>
+		Saya Tinggal Di Rumah <br>
+		Olahraga yang saya suka, dulunya badminton
+		";
+	}
+}
+		
